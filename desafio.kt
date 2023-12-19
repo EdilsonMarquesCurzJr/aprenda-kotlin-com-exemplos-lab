54125330719af7562bb4ada1ce7225aa2f2b61db
Edilson Marques <104401369+EdilsonMarquesCurzJr@users.noreply.github.com>
2023-12-19 17:40:24 -0300

    Modificando para ficar unico

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,32 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val name: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario) {
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        for( i in usuario){
+            inscritos.add(i)
+            println("$i matriculado \ncom conteúdo $conteudos.toString .\n")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+//     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+//     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+		
+    val aluno1= Usuario(name= "Edilson", email= "edilson123@gmail.com")
+    val aluno2= Usuario(name= "José", email= "jose123@gmail.com")
+    val aluno3= Usuario(name= "Maria", email= "maria123@gmail.com")
+    val conteudo = mutableListOf(ConteudoEducacional("Aprendizado com Kotlin",duracao= 60 , Nivel.BASICO))
+    val formacao = Formacao("koltin", conteudo )
+    formacao.matricular(aluno1, aluno2, aluno3)
+    
 }
